f13fd4eddda0e6453cba2ef6f483b8b50ba19174
y-fuyusawa <yuichiro.fuyusawa@officefuyusawa.jp>
2021-09-23 16:34:36 +0900

    Make components touchable

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import {
+  View, Text, StyleSheet,
+} from 'react-native';
 
 export default function AppBar() {
   return (
